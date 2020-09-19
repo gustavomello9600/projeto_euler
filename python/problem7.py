dcd1db1db5c3a3459aa8d084d956487f0effaea1
gustavomello9600 <gustavomelloenciv@gmail.com>
2020-09-19 23:49:05 +0300

    Solution of Problem 7 implemented in Python.
    Problem 6 turned out so trivial that it will be ignored on purpose.

---
+++ python/problem7.py
@@ -0,0 +1,25 @@
+from itertools import cycle
+
+relevant_primes = [2, 3, 5, 7, 11, 13, 17, 19]
+i = 8
+
+
+def is_prime(n):
+    for p in relevant_primes:
+        if n % p == 0:
+            return False
+    return True
+
+
+p = 19
+steps = cycle([2, 2, 4, 2])
+
+while i < 10001:
+    p += next(steps)
+
+    if is_prime(p):
+        relevant_primes.append(p)
+        i += 1
+        
+
+print(f"10001st prime is {p}")
