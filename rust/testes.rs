068e4652f67958eb43870cebcf335cda80376bb9
gustavomello9600 <gustavomelloenciv@gmail.com>
2020-09-24 23:05:40 -0300

    Adicionado arquivo para experimentação com o Rust.

---
+++ rust/testes.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let s = String::from("12345");
+    let vec = s.chars().collect::<Vec<char>>();
+    println!("{:?}", s.chars().collect::<Vec<char>>());
+
+    for c in &vec[1..4] {
+        println!("{:?}", c);
+    }
+}
