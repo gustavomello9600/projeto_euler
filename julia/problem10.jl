64a31a1b6c4445c72dc73efea00d2efe79e7c089
gustavomello9600 <gustavomelloenciv@gmail.com>
2020-10-01 12:22:47 -0300

    Finished Julia implemention of Problem 10

--- julia/problem10.jl
+++ julia/problem10.jl
@@ -1,6 +1,23 @@
 function sum_primes_until(n)
     numbers = Array(2:n)
-    return numbers
+    limit = floor(Int, √n)
+    sieve = ones(Bool, n - 1)
+
+    for i in 4:2:n
+        sieve[i - 1] = false
+    end
+
+    for i in 3:2:limit
+        if sieve[i - 1]
+            for j in i^2:2*i:n
+                sieve[j - 1] = false
+            end
+        end
+    end
+
+    primes = numbers[sieve]
+
+    return sum(primes)
 end
 
-sum_primes_until(2000000)
+println("Sum of primes up to 2,000,000: $(sum_primes_until(2_000_000))")
